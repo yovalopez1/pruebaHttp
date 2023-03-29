fdf25aa3e846091fcee43d53b9e8577962dd99b7
Jovany Lopez <yova1128lopez@gmail.com>
2023-03-29 11:45:43 -0500

    inicio repositorio prueba

---
+++ src/main/java/com/co/globant/pruebaHttp/PruebaHttpApplication.java
@@ -0,0 +1,13 @@
+package com.co.globant.pruebaHttp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaHttpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaHttpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/co/globant/pruebaHttp/PruebaHttpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.globant.pruebaHttp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaHttpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
